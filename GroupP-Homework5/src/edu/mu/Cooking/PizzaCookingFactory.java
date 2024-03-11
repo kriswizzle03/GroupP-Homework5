fb0f24f2ecdcbdf1388bdc370858f302e56586c1
kriswizzle03 <krislmor21@gmail.com>
2024-03-10 21:48:27 -0500

    added switch case to retunr pizza based on pizzaType parameter

--- GroupP-Homework5/src/edu/mu/Cooking/PizzaCookingFactory.java
+++ GroupP-Homework5/src/edu/mu/Cooking/PizzaCookingFactory.java
@@ -1,10 +1,38 @@
 package edu.mu.Cooking;
 
+import edu.mu.pizzaTypes.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.mu.pizza.*;
+
+import edu.mu.pizza.*;
+
+
 import edu.mu.pizza.AbstractPizza;
 import edu.mu.pizzaTypes.PizzaType;
 
-public class PizzaCookingFactory {
+//creates AbstractPizza instance and instantiates it according to pizzaType parameter
+//sets pizzaOrderID of the pizza using current orderIDCounter
+public class PizzaCookingFactory{
+	
 	public AbstractPizza createPizza(PizzaType pizzaType) {
+		
+		double orderIDCounter = orderIDCounter++;
+		switch(pizzaType) {
+			case MARGHERITA:
+				return new MargheritaPizza();
+			case HAWAIIAN:
+				return new HawaiianPizza();
+			case SUPREME:
+				return new SupremePizza();
+			case VEGETARIAN:
+				return new VegetarianPizza();
+			default:
+				System.out.println("Invalid pizza type!");
+				break;
+		}
 		return null;
 		
 	}
